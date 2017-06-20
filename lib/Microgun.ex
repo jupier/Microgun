2f6999d3e8e9dfbf48de0958c998955cb73f6568
Julien PIEROPAN <julien.pieropan@teads.tv>
2017-06-20 09:44:20 +0200

    [Hello :World!] Init project

---
+++ lib/Microgun.ex
@@ -0,0 +1,18 @@
+defmodule Microgun do
+  @moduledoc """
+  Documentation for Microgun.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Microgun.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/Microgun_test.exs
@@ -0,0 +1,8 @@
+defmodule MicrogunTest do
+  use ExUnit.Case
+  doctest Microgun
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
